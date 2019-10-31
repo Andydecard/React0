d452db9a72ab64f9338aae824ce638a6bb2aa1b4
Andydecard <andydecard@yandex.ru>
2019-10-31 14:13:00 +0300

    Lesson__4

--- static_src/Components/MessageField.jsx
+++ static_src/Components/MessageField.jsx
@@ -27,6 +27,8 @@ export default class MessageField extends React.Component {
         }
     }
 
+
+
     handleSendMessage = () => {
         const { messages, input } = this.state;
         this.setState({
@@ -59,6 +61,7 @@ export default class MessageField extends React.Component {
 
                 <div style={ { width: '100%', display: 'flex' } }>
                     <TextField
+                        className="input"
                         name="input"
                         fullWidth={ true }
                         hintText="Введите сообщение"
@@ -68,7 +71,7 @@ export default class MessageField extends React.Component {
                         onKeyUp={ (event) => this.handleKeyUp(event, this.state.input) }
                     />
                     <FloatingActionButton onClick={ () => this.handleSendMessage(this.state.input) }>
-                        <SendIcon />
+                        <SendIcon className="SendIcon"/>
                     </FloatingActionButton>
                 </div>
 
